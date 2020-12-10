c0f7aa29b6200e2f8ea51094460af1e22dae4c69
Semih Celik <s.celikce@gmail.com>
2020-12-10 18:40:45 +0300

    a

--- .github/actions/check-author/index.js
+++ .github/actions/check-author/index.js
@@ -3,6 +3,8 @@ const github = require('@actions/github');
 
 
 try {
+    const emailSuffix = core.getInput('email_suffix');
+
     const {commits} = github.context.payload
 
     const commitEmails = commits.map(c => c.author.email)
@@ -10,6 +12,16 @@ try {
     console.log("EMAILS: ", commitEmails)
 
     console.log("GITHUB: ", JSON.stringify(github))
+
+    console.log(`Check author email if ends with ${emailSuffix} `)
+    const invalidEmails = commitEmails.filter(email => !email.endsWith(emailSuffix))
+
+    if (invalidEmails.length === 0) {
+        return console.log('Author email is valid')
+    }
+    const errorMessage = `Author email is invalid. Found: ${invalidEmails.join(',')} it should be MAIL_PREFIX@${emailSuffix}`
+    throw Error(errorMessage)
+
 } catch (e) {
     core.setFailed("An error occurred: " + e.message)
 }
